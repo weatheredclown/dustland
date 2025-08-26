6843c4a6eddbcc43205412f0d47ef8e88728d763
Tim Laubach <tim.laubach@gmail.com>
2025-08-26 11:09:54 -0400

    fix: update combat health bars

--- test/core.test.js
+++ test/core.test.js
@@ -1066,6 +1066,29 @@ test('fallen party members are revived after combat', async () => {
   assert.ok(party[0].hp >= 1);
 });
 
+test('combat hp bars update after damage', async () => {
+  party.length = 0;
+  player.inv.length = 0;
+  const m1 = new Character('p1','P1','Role');
+  m1.hp = 2;
+  m1.maxHp = 2;
+  party.addMember(m1);
+
+  const resultPromise = openCombat([
+    { name:'E1', hp:2, maxHp:2 }
+  ]);
+
+  handleCombatKey({ key:'Enter' });
+  const enemyHp = combatEnemies.children[0].children[1].children[0].style.width;
+  const memberHp = combatParty.children[0].children[1].children[0].style.width;
+  assert.strictEqual(enemyHp, '50%');
+  assert.strictEqual(memberHp, '50%');
+
+  handleCombatKey({ key:'Enter' });
+  const res = await resultPromise;
+  assert.strictEqual(res.result, 'loot');
+});
+
 test('combat menu can be clicked', async () => {
   party.length = 0;
   player.inv.length = 0;
