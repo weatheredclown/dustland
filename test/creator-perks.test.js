79c0cb1c60424c3d0059ca9add7e224342c3bdff
Tim Laubach <tim.laubach@gmail.com>
2025-08-30 13:14:12 -0400

    test: cover specialization and quirk bonuses

---
+++ test/creator-perks.test.js
@@ -0,0 +1,42 @@
+import assert from 'node:assert';
+import { test } from 'node:test';
+import fs from 'node:fs/promises';
+import vm from 'node:vm';
+import { JSDOM } from 'jsdom';
+
+const code = await fs.readFile(new URL('../scripts/dustland-core.js', import.meta.url), 'utf8');
+
+function setup(){
+  const html = `<body><div id="mapname"></div><div id="creator"></div><div id="ccStep"></div><div id="ccRight"></div><div id="ccHint"></div><button id="ccBack"></button><button id="ccNext"></button><div id="ccPortrait"></div><button id="ccStart"></button><button id="ccLoad"></button></body>`;
+  const dom = new JSDOM(html);
+  const party=[]; party.flags={}; party.map='world'; party.x=0; party.y=0;
+  const inv=[];
+  const context={
+    window:dom.window,
+    document:dom.window.document,
+    EventBus:{ on:()=>{}, emit:()=>{} },
+    baseStats:()=>({STR:4,AGI:4,INT:4,PER:4,LCK:4,CHA:4}),
+    makeMember:(id,name,role)=>({id,name,role,stats:{},special:[]}),
+    joinParty:m=>{ party.push(m); },
+    addToInv:i=>{ inv.push(i); },
+    rand:()=>0,
+    log:()=>{},
+    party,
+    Math:Object.assign(Object.create(Math),{random:()=>0})
+  };
+  vm.createContext(context);
+  vm.runInContext(code,context);
+  return {context,inv};
+}
+
+test('specialization and quirk bonuses apply',()=>{
+  const {context,inv}=setup();
+  context.openCreator();
+  vm.runInContext("building.spec='Gunslinger'; building.quirk='Lucky Lint'", context);
+  const member=vm.runInContext('finalizeCurrentMember()', context);
+  assert.strictEqual(member.stats.AGI,5);
+  assert.strictEqual(member.stats.LCK,5);
+  const ids=inv.map(i=>i.id);
+  assert(ids.includes('pipe_rifle'));
+  assert(ids.includes('lucky_coin'));
+});
