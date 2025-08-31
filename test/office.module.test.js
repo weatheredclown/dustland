57350d76b98797973fddb53e36b965327774d901
Tim Laubach <tim.laubach@gmail.com>
2025-08-31 10:28:06 -0400

    feat: add candy bar to office vending

--- test/office.module.test.js
+++ test/office.module.test.js
@@ -6,6 +6,11 @@ import { fileURLToPath } from 'node:url';
 import vm from 'node:vm';
 import { JSDOM } from 'jsdom';
 
+function extractOpenShop(code) {
+  const m = code.match(/function openShop\(npc\) {[\s\S]*?shopOverlay\.focus\(\);\r?\n}/);
+  return m && m[0];
+}
+
 test('office module boards castle and unboards via dialog', () => {
   const __dirname = path.dirname(fileURLToPath(import.meta.url));
   const file = path.join(__dirname, '..', 'modules', 'office.module.js');
@@ -32,6 +37,30 @@ test('office module places Boots of Speed near forest entry', () => {
   assert.match(src, /id: 'boots_of_speed'/);
   assert.match(src, /x: 3,\s*y: WORLD_MIDY/);
   assert.match(src, /mods: \{ AGI: 5, move_delay_mod: 0\.5 \}/);
+});
+
+test('startGame grants 10 scrap', () => {
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const file = path.join(__dirname, '..', 'modules', 'office.module.js');
+  const src = fs.readFileSync(file, 'utf8');
+  const fnMatch = src.match(/startGame = function \(\) {[\s\S]*?};\n/);
+  assert(fnMatch);
+  global.player = { scrap: 0 };
+  let hudCalled = false;
+  global.updateHUD = () => { hudCalled = true; };
+  global.applyModule = () => ({});
+  global.registerItem = (it) => it;
+  global.setPartyPos = () => {};
+  global.setMap = () => {};
+  global.refreshUI = () => {};
+  global.log = () => {};
+  global.itemDrops = [];
+  global.interiors = {};
+  global.OFFICE_MODULE = { worldGen: () => ({}), start: { x: 0, y: 0, map: 'floor1' }, postLoad: () => {} };
+  vm.runInThisContext(fnMatch[0]);
+  startGame();
+  assert.equal(player.scrap, 10);
+  assert(hudCalled);
 });
 
 test('office worker lends scrap when low and missing badge', () => {
@@ -101,11 +130,6 @@ test('vending machine buys access card for scrap', () => {
   global.addToInv = () => true;
   global.getItem = () => accessCard;
 
-  function extractOpenShop(code) {
-    const m = code.match(/function openShop\(npc\) {[\s\S]*?shopOverlay\.focus\(\);\r?\n}/);
-    return m && m[0];
-  }
-
   const eng = fs.readFileSync(path.join(__dirname, '..', 'scripts', 'dustland-engine.js'), 'utf8');
   const openShopCode = extractOpenShop(eng);
   vm.runInThisContext(openShopCode);
@@ -116,6 +140,37 @@ test('vending machine buys access card for scrap', () => {
   sellBtn.onclick();
   assert.strictEqual(player.scrap, accessCard.value);
   assert.strictEqual(player.inv.length, 0);
+});
+
+test('vending machine sells Dusty Candy Bar', () => {
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const file = path.join(__dirname, '..', 'modules', 'office.module.js');
+  const src = fs.readFileSync(file, 'utf8');
+  const npcMatch = src.match(/id: 'vending',[\s\S]*?shop: \{ inv: \[ \{ id: 'dusty_candy' \} \] \}/);
+  assert(npcMatch);
+  const vending = { name: 'Vending Machine', vending: true, shop: { inv: [ { id: 'dusty_candy' } ] } };
+  const itemMatch = src.match(/\{ id: 'dusty_candy',[\s\S]*?\}\s*\},/);
+  assert(itemMatch);
+  const candy = vm.runInThisContext('(' + itemMatch[0].slice(0, -1) + ')');
+
+  const dom = new JSDOM(
+    '<div id="shopOverlay"><div class="shop-window"><header><div id="shopName"></div><button id="closeShopBtn"></button></header><div class="shop-panels"><div id="shopBuy" class="slot-list"></div><div id="shopSell" class="slot-list"></div></div></div></div>'
+  );
+  global.window = dom.window;
+  global.document = dom.window.document;
+  global.requestAnimationFrame = () => {};
+  global.CURRENCY = 's';
+  global.player = { scrap: 10, inv: [] };
+  global.addToInv = () => true;
+  global.removeFromInv = () => {};
+  global.updateHUD = () => {};
+  global.getItem = (id) => (id === candy.id ? candy : null);
+  const eng = fs.readFileSync(path.join(__dirname, '..', 'scripts', 'dustland-engine.js'), 'utf8');
+  const openShopCode = extractOpenShop(eng);
+  vm.runInThisContext(openShopCode);
+  openShop(vending);
+  const buySpan = document.querySelector('#shopBuy .slot span');
+  assert(buySpan && buySpan.textContent.includes('Dusty Candy Bar'));
 });
 
 test('office worker hides loan if you still have your badge', () => {
