da3bab6161d1811cb35c99cd45e77315b0c3c601
Tim Laubach <tim.laubach@gmail.com>
2025-09-06 11:23:44 -0400

    taller radio tower

--- modules/radio-tower.module.js
+++ modules/radio-tower.module.js
@@ -2,7 +2,12 @@ const DATA = `{
   "name": "Radio Tower",
   "start": { "map": "tower", "x": 1, "y": 1 },
   "interiors": [
-    { "id": "tower", "w": 3, "h": 3, "grid": ["🧱🧱🧱","🧱⬜🧱","🧱🧱🧱"], "entryX": 1, "entryY": 1 }
+    { "id": "tower", "w": 3, "h": 4, "grid": [
+"🧱🧱🧱",
+"🧱⬜🧱",
+"🧱⬜🧱",
+"🧱🧱🧱"
+], "entryX": 1, "entryY": 1 }
   ],
   "events": [
     { "map": "tower", "x": 1, "y": 1, "events": [ { "when": "enter", "effect": "openRadio" } ] }
