c30e45e8bfc593087fed565d317706ba39be88de
Tim Laubach <tim.laubach@gmail.com>
2025-10-22 20:18:24 -0400

    fix: type starter data and profile schema globals

--- data/items/starter.js
+++ data/items/starter.js
@@ -1,12 +1,10 @@
-// @ts-nocheck
-(function () {
-    globalThis.Dustland = globalThis.Dustland || {};
-    globalThis.Dustland.starterItems = [
-        {
-            id: 'starter_canteen',
-            name: 'Canteen',
-            type: 'misc',
-            use: { type: 'hydrate', amount: 2, text: 'You take a drink.' }
-        }
-    ];
-})();
+const starterItems = [
+    {
+        id: 'starter_canteen',
+        name: 'Canteen',
+        type: 'misc',
+        use: { type: 'hydrate', amount: 2, text: 'You take a drink.' }
+    }
+];
+const dustland = (globalThis.Dustland ?? (globalThis.Dustland = {}));
+dustland.starterItems = starterItems;
--- ts-src/data/items/starter.ts
+++ ts-src/data/items/starter.ts
@@ -1,12 +1,26 @@
-// @ts-nocheck
-(function(){
-  globalThis.Dustland = globalThis.Dustland || {};
-  globalThis.Dustland.starterItems = [
-    {
-      id: 'starter_canteen',
-      name: 'Canteen',
-      type: 'misc',
-      use: { type: 'hydrate', amount: 2, text: 'You take a drink.' }
-    }
-  ];
-})();
+type StarterItem = {
+  id: string;
+  name: string;
+  type: 'misc';
+  use: {
+    type: 'hydrate';
+    amount: number;
+    text: string;
+  };
+};
+
+const starterItems: StarterItem[] = [
+  {
+    id: 'starter_canteen',
+    name: 'Canteen',
+    type: 'misc',
+    use: { type: 'hydrate', amount: 2, text: 'You take a drink.' }
+  }
+];
+
+type StarterItemsNamespace = DustlandNamespace & {
+  starterItems: StarterItem[];
+};
+
+const dustland = (globalThis.Dustland ??= {}) as StarterItemsNamespace;
+dustland.starterItems = starterItems;
